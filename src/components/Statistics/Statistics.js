bcfc1f46ac61e56407dbf326ba39559dc860080d
smirnypavel <89521405+smirnypavel@users.noreply.github.com>
2023-03-02 18:43:27 +0200

    Well-done

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,5 +1,5 @@
-// import PropTypes from 'prop-types';
-import { FcLike } from "react-icons/fc";
+import PropTypes from 'prop-types';
+import { FcLike } from 'react-icons/fc';
 import Notification from 'components/Notification';
 import styled from './Statistic.module.css';
 
@@ -8,7 +8,9 @@ const Statistics = ({ good, neutral, bad, total, percent }) => {
     <>
       {total > 0 && (
         <ul className={styled.ul}>
-          <li className={styled.li}>Good:{good} <FcLike/></li>
+          <li className={styled.li}>
+            Good:{good} <FcLike />
+          </li>
           <li className={styled.li}>Neutral:{neutral}</li>
           <li className={styled.li}>Bad:{bad}</li>
           <li className={styled.li}>Total:{total}</li>
@@ -18,6 +20,14 @@ const Statistics = ({ good, neutral, bad, total, percent }) => {
       {total === 0 && <Notification message="No feadback given" />}
     </>
   );
+};
+
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  percent: PropTypes.number.isRequired,
 };
 
 export default Statistics;
